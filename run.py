d21d038434f18380aed7d0b91796203b0db14f61
FALY <faliarisoazafindrasojacharlotr@gmail.com>
2025-08-14 15:15:48 +0300

    authentification

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from routes import app
+from flask import Flask
+from app.routes.routes import routes_bp
 
-if __name__ == '__main__':
+app = Flask(__name__)
+app.register_blueprint(routes_bp)  # pas de url_prefix pour que "/" marche
+
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
